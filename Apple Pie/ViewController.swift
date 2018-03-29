8e06cfef50c5d0455d842c6bdd0b07fff28a48d6
student <student@student-16.local>
2018-03-29 14:51:02 +0300

    first version

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var treeImageView:
+    UIImageView!
+    @IBOutlet weak var correctWordLabel:
+    UILabel!
+    @IBOutlet weak var scoreLabel:
+    UILabel!
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func
+        didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-}
+ }
 
